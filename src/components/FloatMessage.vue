aa7d01205749b6c4473e2aa34f42691c935e8793
daixii83 <daixii83@gmail.com>
2024-02-26 09:49:27 +0800

    add component

---
+++ src/components/FloatMessage.vue
@@ -0,0 +1,33 @@
+<template>
+    <transition name="fade">
+        <div
+            v-if="message"
+            class="fixed bottom-4 right-4 bg-primary-400 text-mixed-200 p-4 rounded-md"
+        >
+            {{ message }}
+        </div>
+    </transition>
+</template>
+
+<script setup>
+    const message = ref('');
+
+    const showMessage = (newMessage) => {
+        message.value = newMessage;
+        setTimeout(() => {
+            message.value = '';
+        }, 3000);
+    };
+</script>
+
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.5s;
+    }
+
+    .fade-enter-from,
+    .fade-enter-to {
+        opacity: 0;
+    }
+</style>
